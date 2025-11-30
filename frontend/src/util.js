b2a621d3d88c760e56b570067465e0fdb1f6d62b
Yasin Akkus <akkusoft@gmail.com>
2025-11-30 21:35:17 +0100

    changed API_BASE_URL to back as in video tutorial

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  "https://5416d366-b7b9-4253-991e-945055b9e3ab-dev.e1-eu-west-cdp.choreoapis.dev/choose-your-own-adventure/backend/v1/api";
+  "/choreo-apis/choose-your-own-adventure/backend/v1/api";
