69ba3f341369023f366e2bee3c51147c115f04c3
Yasin Akkus <akkusoft@gmail.com>
2025-11-30 12:34:49 +0100

    update backend for openai mservice

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -8,6 +8,7 @@ from core.prompts import STORY_PROMPT
 from models.story import Story, StoryNode
 from core.models import StoryLLMResponse, StoryNodeLLM
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -15,6 +16,12 @@ class StoryGenerator:
 
     @classmethod
     def _get_llm(cls):
+        openai_api_key = os.getenv("CHOREO_OPENAI_CONNECTION_OPENAI_API_KEY")
+        serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
+
+        if openai_api_key and serviceurl:
+            return ChatOpenAI(model="gpt-4o-mini", openai_api_key=openai_api_key, serviceurl=serviceurl)
+        
         return ChatOpenAI(model_name="gpt-4-turbo")
     
     @classmethod
