6c7028c5b4dd782201513bba2d8243f8cca5c1d8
Yasin Akkus <akkusoft@gmail.com>
2025-11-30 19:52:27 +0100

    changed serviceurl keyword to base_url

--- backend/core/story_generator.py
+++ backend/core/story_generator.py
@@ -20,7 +20,7 @@ class StoryGenerator:
         serviceurl = os.getenv("CHOREO_OPENAI_CONNECTION_SERVICEURL")
 
         if openai_api_key and serviceurl:
-            return ChatOpenAI(model="gpt-4o-mini", openai_api_key=openai_api_key, serviceurl=serviceurl)
+            return ChatOpenAI(model="gpt-4o-mini", openai_api_key=openai_api_key, base_url=serviceurl)
         
         return ChatOpenAI(model="gpt-4o-mini")
     
